2b3b8b3a98476de781bc59c5bfd3df20873b5f48
Wellington Cavalcante <wellarts@gmail.com>
2025-08-05 17:00:22 -0300

    documentos

--- app/Filament/Resources/DocumentoResource.php
+++ app/Filament/Resources/DocumentoResource.php
@@ -19,7 +19,7 @@ class DocumentoResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    protected static ?string $navigationGroup = 'Declarações/Outros';
+    protected static ?string $navigationGroup = 'Atendimentos';
 
     protected static string $modalWidth = 'full';
 
